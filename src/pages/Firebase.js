14f8fc1e09aeaed931712836e9b4cfcf7467d815
Ham9911 <mhammad8633@gmail.com>
2021-12-01 19:50:38 +0500

    Initial commit

---
+++ src/pages/Firebase.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { getFirestore, doc, setDoc, getDoc, addDoc, collection, getDocs, query,where } from "firebase/firestore";
+
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyDvVYViIxakEjH7PdEd3RJvrkGG22E4U5A",
+  authDomain: "friends-app-5751e.firebaseapp.com",
+  projectId: "friends-app-5751e",
+  storageBucket: "friends-app-5751e.appspot.com",
+  messagingSenderId: "138260231048",
+  appId: "1:138260231048:web:b61bd4506e4f6cb0d5003d"
+});
+
+const auth = getAuth();
+const db = getFirestore(firebaseApp);
+
+export {
+    auth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
+
+    db,
+    doc,
+    setDoc,
+    getDoc,
+    addDoc,
+    collection,
+    getDocs,
+    query,
+    where
+};
